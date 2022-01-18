2422e5e6b1e4ed5ed47417670da9b792c5d1f701
serref jaguraga <reef163@gmail.com>
2022-01-17 22:21:28 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+ 
+const newCats = [...cats, "Broom"];
+function appendCat(){
+    return newCats;
+}
+
+const addCat = ["Arnold", ...cats];
+function prependCat(){
+    return addCat;
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
